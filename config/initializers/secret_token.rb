126bf9e8b3cc15ed985be70defcc7655e3d6e8e8
emapstone <ehardy@2025labs.com>
2013-09-23 12:21:20 -0700

    created new rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site2025::Application.config.secret_key_base = '8ce136c8e8bd284112c1382135a25cfd42acf7c12aa35b0ae44bf9a731063695ce943f25ba3fea6c0d677887a54898ef5a1a1d7871e666f18260a1a4ab595237'
