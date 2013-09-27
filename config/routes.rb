170c1169d4ba9d5537f7bf91cabaa8e52e547866
emapstone <ehardy@2025labs.com>
2013-09-27 08:18:42 -0700

    added css and static pages vies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Site2025::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about_us"
-  get "static_pages/contact"
+
+    get "static_pages/home"
+    get "static_pages/about_us"
+    get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
   describe "Home page" do
-
-      it "should have the content '2025 Labs'" do
-        visit '/static_pages/home'
-        expect(page).to have_content('2025 Labs')
-      end
+    it "should have the content '2025 Labs'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('2025 Labs')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("2025 Labs | Home")
+    end
+    
   end
+    
+    
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the right title" do
+        visit '/static_pages/home'
+        expect(page).to have_title("2025 Labs | Contact")
+    end
+    
+  end
+  
+  describe "About Us page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('About')
+    end
+    
+    it "should have the right title" do
+        visit '/static_pages/home'
+        expect(page).to have_title("2025 Labs | About Us")
+    end
+    
+    
+  end
+    
+    
+end
