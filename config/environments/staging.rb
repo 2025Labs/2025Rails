4e8b9722e6a1fba24d6e5ab0e755ead4d9936f16
Steve Foster <srfoster@speakeasy.net>
2013-10-08 12:24:52 -0700

    assets pipeline setups.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -28,7 +28,7 @@ Site2025::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+config.assets.compress = true
   # Generate digests for assets URLs.
   config.assets.digest = true
 
