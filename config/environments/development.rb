4e8f6fed13cc3a87375efb30ea31ca7ddc315a43
Melissa Liu <mliu@2025labs.com>
2017-09-21 13:30:19 -0700

    changes in application.js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ Site2025::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 end
